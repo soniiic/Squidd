46152a5267c3bd0b03282d9938320b85d6e6750f
Dan Cooper <soniiic@outlook.com>
2017-08-12 21:44:25 +0100

    Add a monstrously big MVC application

---
+++ src/Squidd.Commander.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Squidd.Commander.Web.MvcApplication" Language="C#" %>
---
+++ src/Squidd.Commander.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Squidd.Commander.Web.Startup))]
+namespace Squidd.Commander.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

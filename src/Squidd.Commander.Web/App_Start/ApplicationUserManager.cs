fc412fed4e06946924e1dfa33d519546db5e0a65
Dan Cooper <soniiic@outlook.com>
2017-08-12 21:45:08 +0100

    remove all unused usings

--- src/Squidd.Commander.Web/App_Start/ApplicationUserManager.cs
+++ src/Squidd.Commander.Web/App_Start/ApplicationUserManager.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
